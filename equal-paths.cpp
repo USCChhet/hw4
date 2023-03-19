40f0fda2ea945ed1d1f967dc8225892c5edb0cf8
Chhai Chhet <chhet@usc.edu>
2023-03-19 06:19:21 +0000

    update

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -1,18 +1,68 @@
 #ifndef RECCHECK
 //if you want to add any #includes like <iostream> you must do them here (before the next endif)
-
+#include <algorithm>
 #endif
 
 #include "equal-paths.h"
+
 using namespace std;
 
-
-// You may add any prototypes of helper functions here
-
-
-bool equalPaths(Node * root)
+int getHeight(Node* curr)
 {
-    // Add your code below
+	int heightL = -1;
+	int heightR = -1;
 
+	if(curr == NULL)
+	{
+		return 0;
+	}
+
+	if(curr->left != NULL)
+	{
+		heightL = getHeight(curr->left);
+	}
+
+	if(curr->right != NULL)
+	{
+		heightR = getHeight(curr->right);
+	}
+
+	return  1 + std::max(heightL, heightR);
 }
+
+bool dopaths(Node* root)
+{
+    int right = getHeight(root->right);
+    int left = getHeight(root->left);
+
+    // Add your code below
+    if(right != left)
+    {
+        return false;
+    }
+
+   //Check if there are subtrees under us
+	if(right == left)
+	{
+		equalPaths(root->left);
+		equalPaths(root->right);
+	}
+	
+	//If all nodes are balanced return true!
+	return true;
+}
+
+bool equalPaths(Node* root)
+{
+    if(root == NULL)
+    {
+        return true;
+    }
+
+    return dopaths(root);
+}
+
+
+
+
 
